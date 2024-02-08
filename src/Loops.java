68324eb942cc1d6cf6626bb2e6f20d2fca752439
Kamil Najdek <259011@student.pwr.edu.pl>
2024-02-08 18:37:56 +0100

    simple loop usage

---
+++ src/Loops.java
@@ -0,0 +1,10 @@
+public class Loops {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0){
+                System.out.println("Liczba " + i + " jest podzielna przez 3.");
+            }
+        }
+    }
+}
+
