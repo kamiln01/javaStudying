af3f9cdd99b01d9a8e430b3eaa393fa12a803b30
Kamil Najdek <259011@student.pwr.edu.pl>
2024-02-19 13:42:11 +0100

    Optimized lists

--- src/Fibonacci/Fibonacci.java
+++ src/Fibonacci/Fibonacci.java
@@ -9,12 +9,13 @@ public class Fibonacci {
         System.out.println("Which index of Fibonacci sequence do you want to check");
         int number = scanner.nextInt();
         // n1 = 1, n2 = 2, n3 = 3, n4 = 5, n5 = 8, n6 = 13, n7 = 21
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(1);
         for (int i = 2; i < number; i++) {
-            list.add(list.get(i-2)+list.get(i-1));
+            list.add(list.get(0)+list.get(1));
+            list.remove(list.getFirst());
         }
-        System.out.println(list.getLast());
+        System.out.println(number + ". fibonacci sequence number is " + list.getLast());
     }
 }
